4209c8facf4b5ea028b44d9589ab2a888ee0ba84
Philane Sigwebela <kaizer.pk@gmail.com>
2026-09-03 16:42:36 +0200

    Add and configure route for the application

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,6 +9,8 @@ import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { RouterProvider } from 'react-router'
+import { router } from './app/router/Routes'
 
 const queryClient = new QueryClient();
 
@@ -16,7 +18,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools />
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>,
 )
