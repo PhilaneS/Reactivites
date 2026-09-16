f1adc4587799e8aeed9645eac33a66698259f693
Philane Sigwebela <kaizer.pk@gmail.com>
2026-09-16 18:36:42 +0200

    Updated deployment workflow and fixed map icon issue

--- client/src/app/shared/componets/MapComponent.tsx
+++ client/src/app/shared/componets/MapComponent.tsx
@@ -1,6 +1,8 @@
 
-import { MapContainer, TileLayer, Popup, Marker } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css'
+import { MapContainer, TileLayer, Popup, Marker } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { Icon } from 'leaflet';
+import markerIconPng from 'leaflet/dist/images/marker-icon.png'
 
 type Props = {
     position: [number, number],
@@ -15,7 +17,7 @@ export default function MapComponent({ position, vanue }: Props) {
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={position}>
+                <Marker position={position} icon={new Icon({ iconUrl: markerIconPng })} >
                     <Popup>
                         {vanue}
                     </Popup>
