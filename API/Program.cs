e536b47efc04a6fcb3888596eeddc77fd2aa15e0
Philane Sigwebela <kaizer.pk@gmail.com>
2026-09-21 19:46:24 +0200

    Removed client from being served from API

--- API/Program.cs
+++ API/Program.cs
@@ -118,13 +118,13 @@ app.UseMiddleware<ExceptionMiddleware>();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.UseDefaultFiles();
-app.UseStaticFiles();
+//app.UseDefaultFiles();
+//app.UseStaticFiles();
 
 app.MapControllers();
 
 app.MapGroup("api").MapIdentityApi<User>();
 app.MapHub<CommentHub>("/comments");
-app.MapFallbackToController("Index", "Fallback");
+//app.MapFallbackToController("Index", "Fallback");
 
 app.Run();
