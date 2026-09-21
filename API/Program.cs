c1ac39dd70c3ca7121b4be9ce398e1cbce2c317e
Philane Sigwebela <kaizer.pk@gmail.com>
2026-09-21 17:34:11 +0200

    ReAdded legacy React Hosting from API

--- API/Program.cs
+++ API/Program.cs
@@ -117,9 +117,13 @@ app.UseMiddleware<ExceptionMiddleware>();
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.MapControllers();
 
 app.MapGroup("api").MapIdentityApi<User>();
 app.MapHub<CommentHub>("/comments");
+app.MapFallbackToController("Index", "Fallback");
 
 app.Run();
